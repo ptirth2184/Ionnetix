22f11080aaf3aae3b9812662f6a86eeffc30d144
Tirth Patel <ptirth2184@gmail.com>
2026-05-07 23:38:38 +0530

    fix: remove vite and completely shift to Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,11 @@ import { fileURLToPath } from "node:url"
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   turbopack: {
     root: __dirname,
   },
